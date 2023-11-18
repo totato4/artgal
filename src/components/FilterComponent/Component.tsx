18d1f7db52f74e98926d7f01863672033414a409
totato4 <v4.97@list.ru>
2023-11-18 15:02:23 +0300

    little bag fix

--- src/components/FilterComponent/Component.tsx
+++
@@ -1,9 +0,0 @@
-export const Component = ({ children }: any) => {
-  return (
-    <div
-      style={{ background: 'yellow', border: '1px solid black', width: '100%' }}
-    >
-      {children}
-    </div>
-  );
-};
